6002907ce4dc1f96941c6d345212fcf60cb4a185
daly barron <marineorganism@gmail.com>
2015-07-21 21:52:46 +1000

    have a basic working main

--- main.cc
+++ main.cc
@@ -30,5 +30,26 @@ int main(int argc,char * args[])
     {
       printf("window couldn't be created, SDL_Error: %s\n",SDL_GetError());
     }
+    else
+    {
+      //get the window's surface
+      screenSurface = SDL_GetWindowSurface(window);
+
+      //fill the surface with white
+      SDL_FillRect(screenSurface,NULL,SDL_MapRGB(screenSurface->format,0xFF,
+                                                 0xFF,0xFF));
+      //update the surface
+      SDL_UpdateWindowSurface(window);
+
+      //wait two seconds
+      SDL_Delay(2000);
+    }
   }
+  //destroy the window
+  SDL_DestroyWindow(window);
+
+  //quit sdl subsystems
+  SDL_Quit();
+
+  return 0;
 }
